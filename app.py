9ac6bdecb11aa0527215f2fe4aacffef62ea0276
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2024-07-12 12:23:21 +0200

    added json file for storage and POST method

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
+with open('books.json', 'r') as fileobj:
+    books = json.load(fileobj)
 
-@app.route('/api/books', methods=['GET'])
-def get_books():
-    books = [
-        {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
-        {"id": 2, "title": "1984", "author": "George Orwell"}
-    ]
-    return jsonify(books)
+def sync_books(books):
+    updated_books = json.dumps(books)
+    with open('books.json', 'w') as fileobj:
+        fileobj.write(updated_books)
+
+@app.route('/api/books', methods=['GET', 'POST'])
+def handle_books():
+    if request.method == 'POST':
+        # Get the new book data from the client
+        new_book = request.get_json()
+
+        # Generate a new ID for the book
+        new_id = max(book['id'] for book in books) + 1
+        new_book['id'] = new_id
+
+        # Add the new book to our list
+        books.append(new_book)
+
+        # Write to file
+        sync_books(books)
+
+        # Return the new book data to the client
+        return jsonify(new_book), 201
+    else:
+        # Handle the GET request
+        return jsonify(books)
 
 
 if __name__ == "__main__":
